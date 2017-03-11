00cd2f5c0709f2d097ebe0dd618cc13ec67cfcbd
Kevin Wen <cybergodzilla1999@gmail.com>
2017-03-11 17:49:32 -0500

    Add files via upload

---
+++ max-min-difference-in-an-interval.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define MAXN 500010
+using namespace std;
+int N, Q, ori[MAXN];
+int total_subsequence_under_K (int K){
+    deque < pair < int, int > > qmax, qmin;
+    int L = 0, R = 0, ans = 0;
+    while(L < N && R < N){
+        while(!qmin.empty() && qmin.front().second < L){
+            qmin.pop_front();
+        }
+        while(!qmax.empty() && qmax.front().second < L){
+            qmax.pop_front();
+        }
+        if (L==R || max(qmax.front().first, ori[R]) - min(qmin.front().first, ori[R]) <= K){
+            ans += R-L+1;
+            while(!qmin.empty() && qmin.back().first >= ori[R])
+                qmin.pop_back();
+            while(!qmax.empty() && qmax.back().first <= ori[R])
+                qmax.pop_back();
+            qmin.push_back({ori[R],R});
+            qmax.push_back({ori[R],R});
+            R++;
+        }
+        else {
+            L++;
+        }
+    }
+    return ans;
+}
+int main(){
+    cin>>N>>Q;
+    for (int i = 0; i < N; i++){
+        cin>>ori[i];
+    }
+    while(Q--){
+        int low, high;
+        cin>>low>>high;
+        cout<<total_subsequence_under_K(high) - total_subsequence_under_K(low-1)<<"\n";
+    }
+}
